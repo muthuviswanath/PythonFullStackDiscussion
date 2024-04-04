d3969b337ad9e23f86f3c1702de0615b4baf6a63
muthuviswanath <muthuviswanath@outlook.com>
2024-04-04 20:13:54 +0530

    Task

---
+++ DataStructures/Task_04042024.py
@@ -0,0 +1,6 @@
+# List Operations:
+# 1. Create a numerical List
+# 2. Display the second largest element of the list
+# 3. Display the second smallest element of the list
+# 4. Split the given list into two lists by taking the number of elements to be present in the first list
+# 5. Exit
